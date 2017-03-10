bb8a84022aefaed0962282c58404f20ae8da642c
Jorje65 <gexxmwangi@gmail.com>
2017-03-10 15:43:06 +0300

    LandingpageCss

--- js/main.js
+++ js/main.js
@@ -10,7 +10,8 @@
     var $obj = $(this); $(window).scroll(function() { 
         var yPos = -($(window).scrollTop() / $obj.data('speed')); 
         var bgpos = '50% '+ yPos + 'px'; $obj.css('background-position', bgpos ); 
-    }); 
+    });
+		 
 });	
 /*scroll to top*/
 
